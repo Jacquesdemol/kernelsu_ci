3c0a8a14699ea34b3d244e506842998b21d26245
Iury Henrique <101074676+Jacquesdemol@users.noreply.github.com>
2024-09-07 14:35:34 -0300

    Change to miami

--- kernel.sh
+++ kernel.sh
@@ -19,32 +19,30 @@ n=''
 echo "Starting KernelSU CI Builds ($TAG)"
 today=$(date +%y%m%d)
 
-branches=$(curl -s "https://api.github.com/repos/Gabriel2392/android_kernel_samsung_a53x_xy/branches" | jq -r '.[].name')
+branches=$(curl -s "https://api.github.com/repos/Claymore1297/kernel_motorola_miami/branches" | jq -r '.[].name')
 
 HOME="$(pwd)"
-git clone https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r416183b "$HOME/clang-r416183b" --depth=1
+git clone https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-6573524 "$HOME/clang-r383902b1" --depth=1
 git clone https://android.googlesource.com/platform/prebuilts/build-tools "$HOME/build-tools" --depth=1
 
 export CROSS_COMPILE="$HOME/clang-r416183b/bin/aarch64-linux-gnu-"
-export CC="$HOME/clang-r416183b/bin/clang"
-export PLATFORM_VERSION=13
-export ANDROID_MAJOR_VERSION=t
-export PATH="$HOME/clang-r416183b/bin:$PATH"
+export CC="$HOME/clang-r383902b1/bin/clang"
+export PLATFORM_VERSION=14
+export ANDROID_MAJOR_VERSION=u
+export PATH="$HOME/clang-r383902b1/bin:$PATH"
 export PATH="$HOME/build-tools/path/linux-x86:$PATH"
-export TARGET_SOC=s5e8825
 export LLVM=1 LLVM_IAS=1
 export ARCH=arm64
-EXTRA_FLAGS="LOCALVERSION=-KernelSU-${TAG}"
 
 for branch in $branches; do
     rm -rf src
-    git clone https://github.com/Gabriel2392/android_kernel_samsung_a53x_xy -b $branch --depth=1 src || continue
+    git clone https://github.com/Claymore1297/kernel_motorola_miami -b u14.0 --depth=1 src || continue
     cd src
     curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - || continue
     kversion=$(awk -F= '/^VERSION =/ {v=$2} /^PATCHLEVEL =/ {p=$2} /^SUBLEVEL =/ {s=$2} END {gsub(/ /,"",v); gsub(/ /,"",p); gsub(/ /,"",s); print v"."p"."s}' Makefile | sort)
     echo "Building $kversion"
-    make -j$(nproc --all) -C $(pwd) $EXTRA_FLAGS CROSS_COMPILE="$HOME/clang-r416183b/bin/aarch64-linux-gnu-" CC="$HOME/clang-r416183b/bin/clang" TARGET_SOC=s5e8825 LLVM=1 LLVM_IAS=1 ARCH=arm64 PLATFORM_VERSION=13 ANDROID_MAJOR_VERSION=t KBUILD_BUILD_USER=Gabriel KBUILD_BUILD_HOST=KSUCI a53x_defconfig || continue
-    make -j$(nproc --all) -C $(pwd) $EXTRA_FLAGS CROSS_COMPILE="$HOME/clang-r416183b/bin/aarch64-linux-gnu-" CC="$HOME/clang-r416183b/bin/clang" TARGET_SOC=s5e8825 LLVM=1 LLVM_IAS=1 ARCH=arm64 PLATFORM_VERSION=13 ANDROID_MAJOR_VERSION=t KBUILD_BUILD_USER=Gabriel KBUILD_BUILD_HOST=KSUCI || continue
+    make -j$(nproc --all) -C $(pwd) $EXTRA_FLAGS CROSS_COMPILE="$HOME/clang-r383902b1/bin/aarch64-linux-gnu-" CC="$HOME/clang-383902b1/bin/clang" LLVM=1 LLVM_IAS=1 ARCH=arm64 PLATFORM_VERSION=14 ANDROID_MAJOR_VERSION=u || continue
+    make -j$(nproc --all) -C $(pwd) $EXTRA_FLAGS CROSS_COMPILE="$HOME/clang-r383902b1/bin/aarch64-linux-gnu-" CC="$HOME/clang-r383902b1b/bin/clang" LLVM=1 LLVM_IAS=1 ARCH=arm64 PLATFORM_VERSION=14 ANDROID_MAJOR_VERSION=u || continue
     cp "arch/arm64/boot/Image" "${KZIP}/Image"
     cd "$KZIP"
     zip -r "KernelSU_${TAG}-${kversion}.zip" ./
